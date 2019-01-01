457c446c2f15d41179324895bc08e969571d4a88
Grex <punmasterjedi@gmail.com>
2018-12-31 18:32:31 -0700

    feat: deploy apollo-server with now

--- projects/apollo-server/src/index.js
+++ projects/apollo-server/src/index.js
@@ -1,6 +1,8 @@
 import 'dotenv/config'
 import { ApolloServer, gql } from 'apollo-server'
 
+// import { buildServiceDefinition } from '@apollographql/apollo-tools'
+
 const typeDefs = gql`
     type Grex {
         age: Int
@@ -20,8 +22,58 @@ const resolvers = {
     },
 }
 
+// const module1 = {
+//     resolvers: {
+//         Query: {
+//             module1Field: () => 'module1Field',
+//             user: () => ({}),
+//         },
+//     },
+//     typeDefs: gql`
+//         type User {
+//             id: ID
+//         }
+
+//         extend type Query {
+//             module1Field: String
+//             user: User
+//         }
+
+//         extend type User {
+//             module1Field: String
+//         }
+//     `,
+// }
+
+// console.log('service def', buildServiceDefinition([module1]))
+
+// const module2 = {
+//     resolvers: {
+//         Query: {
+//             module2Field: () => 'module2Field',
+//         },
+//         User: {
+//             module2Field: () => 'module2Field',
+//         },
+//     },
+//     typeDefs: gql`
+//         extend type Query {
+//             module2Field: String
+//         }
+
+//         extend type User {
+//             module2Field: String
+//         }
+//     `,
+// }
+
 const server = new ApolloServer({
     cacheControl: true,
+    introspection: true,
+    // modules: [module1, module2],
+    playground: {
+        endpoint: '/_graphql',
+    },
     resolvers,
     tracing: true,
     typeDefs,
