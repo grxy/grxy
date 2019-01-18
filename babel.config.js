f3f5c1b574f9fb9f0420cb48aea0f8be40a10e3a
Andrew Grexa <andrew@grexa.me>
2019-01-18 11:42:20 -0700

    chore: update babel + eslint deps, create preset

--- babel.config.js
+++ babel.config.js
@@ -1,27 +1,3 @@
 module.exports = {
-    plugins: [
-        [
-            'module-resolver',
-            {
-                alias: {
-                    '@grxy/components': '@grxy/components/src',
-                    '@grxy/theme': '@grxy/theme/src',
-                },
-            },
-        ],
-        '@babel/plugin-proposal-class-properties',
-
-        // export * as ns from 'module/path'
-        '@babel/plugin-proposal-export-namespace-from',
-
-        // export default from 'module/path'
-        '@babel/plugin-proposal-export-default-from',
-
-        // required for production minification with Uglify or Terser
-        '@babel/plugin-proposal-object-rest-spread',
-    ],
-    presets: [
-        ['@babel/preset-env', { useBuiltIns: 'usage' }],
-        '@babel/preset-react',
-    ],
+    presets: ['@grxy/babel-preset'],
 }
