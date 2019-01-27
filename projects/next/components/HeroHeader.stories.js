d86b06cc8034eead8c6bb332b6948cb95052e410
Andrew Grexa <andrew@grexa.me>
2019-01-26 18:27:30 -0700

    feat: updated HeroHeader to support wrapped text

--- projects/next/components/HeroHeader.stories.js
+++ projects/next/components/HeroHeader.stories.js
@@ -16,3 +16,13 @@ stories.add('default', () => (
 stories.add('with only a title', () => (
     <HeroHeader title={text('Title', 'This is a title')} />
 ))
+
+stories.add('with a long title', () => (
+    <HeroHeader
+        subtitle={text('Subtitle', 'This is a subtitle')}
+        title={text(
+            'Title',
+            'This is a very long title that should wrap to multiple lines and not be contained on a single line',
+        )}
+    />
+))
