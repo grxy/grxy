f5b6f16a3615175bbcf5ea2bda09e69a372314bb
Andrew Grexa <andrew@grexa.me>
2019-01-26 19:08:00 -0700

    feat: make ThemeProvider controllable

--- .storybook/decorators.js
+++ .storybook/decorators.js
@@ -1,14 +1,37 @@
-import { withKnobs } from '@storybook/addon-knobs'
+import { select, withKnobs } from '@storybook/addon-knobs'
 import { addDecorator } from '@storybook/react'
 import React from 'react'
 
+import { getThemeKey } from '@grxy/next/lib/theme'
+
 import withThemeProvider from '@grxy/next/containers/withThemeProvider'
 
-const ThemeProvider = withThemeProvider(({ children }) => children)
-
-const ThemeDecorator = (storyFn) => <ThemeProvider>{storyFn()}</ThemeProvider>
+let currentTheme
 
 export default () => {
-    addDecorator(ThemeDecorator, module)
+    const ThemeProvider = withThemeProvider(({ children }) => children)
+
+    const ThemeDecorator = (storyFn) => (
+        <ThemeProvider
+            onThemeChange={(theme) => {
+                currentTheme = theme
+            }}
+            themeKey={select(
+                'Theme',
+                {
+                    Orange: 'orange',
+                    Dark: 'dark',
+                    Light: 'light',
+                    Purple: 'purple',
+                },
+                currentTheme || getThemeKey(),
+                'GLOBAL',
+            )}
+        >
+            {storyFn()}
+        </ThemeProvider>
+    )
+
     addDecorator(withKnobs, module)
+    addDecorator(ThemeDecorator, module)
 }
